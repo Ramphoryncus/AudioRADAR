8c54f6d921aec0fcfa866458f260f75f38babd8f
Henry Taylor <htaylor77@hotmail.co.uk>
2026-01-11 09:01:44 +0000

    back to "flat" structure/layout as Rider would not open

--- Source/AudioRADAR/EardarEmitterComponent.cpp
+++ Source/AudioRADAR/EardarEmitterComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Eardar/EardarEmitterComponent.h"
+#include "EardarEmitterComponent.h"
 #include "Components/AudioComponent.h"
 
 UEardarEmitterComponent::UEardarEmitterComponent()
