849748a266c442dc4ace42787f45520f277d5987
Henry Taylor <htaylor77@hotmail.co.uk>
2026-08-12 03:38:02 +0100

    Added Root+Blip canvas and some code to cpp
    Added Root+Blip canvas to WBP_EardarEmitter and some BeginPlay and EndPlay code to EardarEmitterComponent.cpp + .h to help with the interface between C++ and Blueprint

--- Source/AudioRADAR/EardarEmitterComponent.cpp
+++ Source/AudioRADAR/EardarEmitterComponent.cpp
@@ -2,7 +2,10 @@
 
 
 #include "EardarEmitterComponent.h"
+#include "EardarManager.h"
+
 #include "Components/AudioComponent.h"
+#include "EngineUtils.h"
 
 UEardarEmitterComponent::UEardarEmitterComponent()
 {
@@ -12,7 +15,60 @@ UEardarEmitterComponent::UEardarEmitterComponent()
 void UEardarEmitterComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	// Nothing special; the manager will resolve the profile at runtime.
+	/*
+	* Find the EARDAR Manager that has been placed in the current level.
+	*
+	* TActorIterator searches through Actors of the requested type.
+	* In our prototype we expect only ONE AEardarManager in the level,
+	* so once we find the first one we register this emitter and stop.
+	*/
+	if (UWorld* World = GetWorld())
+	{
+		for (TActorIterator<AEardarManager> It(World); It; ++It)
+		{
+			AEardarManager* Manager = *It;
+
+			if (Manager)
+			{
+				/*
+				 * Tell the manager:
+				 *
+				 * "This component is an EARDAR sound source.
+				 *  Please include me when calculating radar blips."
+				 */
+				Manager->RegisterEmitter(this);
+
+				// We only need one manager for this prototype.
+				break;
+			}
+		}
+	}
+}
+
+void UEardarEmitterComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	/*
+	 * Remove this emitter from the manager when the Actor/component
+	 * leaves the world.
+	 *
+	 * This prevents the manager retaining references to objects that
+	 * have been destroyed.
+	 */
+	if (UWorld* World = GetWorld())
+	{
+		for (TActorIterator<AEardarManager> It(World); It; ++It)
+		{
+			AEardarManager* Manager = *It;
+
+			if (Manager)
+			{
+				Manager->UnregisterEmitter(this);
+				break;
+			}
+		}
+	}
+
+	Super::EndPlay(EndPlayReason);
 }
 
 FVector UEardarEmitterComponent::GetEmitterLocation() const
--- Source/AudioRADAR/EardarEmitterComponent.h
+++ Source/AudioRADAR/EardarEmitterComponent.h
@@ -56,4 +56,6 @@ public:
 
 protected:
     virtual void BeginPlay() override;
+    
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 };
